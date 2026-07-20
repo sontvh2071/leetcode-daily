ddba8f8958eee53b3f2ec4b7bde2a5e20764fdb8
sontvh2071 <sonhoang2071.work@gmail.com>
2026-07-20 14:26:21 +0700

    feat: add solution for Shuffle the Array problem with examples and constraints

--- day_001/solution.py
+++ day_001/solution.py
@@ -12,3 +12,16 @@ class Solution:
             pos[num] = i
         
         return []
+    
+    # 1470 - shuffle the array problem
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        shuffled = [0] * (2 * n)
+        shuffled[0] = nums[0]
+        shuffled[1] = nums[n]
+        
+
+        for i in range(1, n):
+            shuffled[i * 2] = nums[i]
+            shuffled[i * 2 + 1] = nums[i + n]
+            
+        return shuffled
