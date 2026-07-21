631b96680772e9ab2d4ddae09975eac19066ae84
sontvh2071 <sonhoang2071.work@gmail.com>
2026-07-21 12:37:45 +0700

    feat: add solution for Best Time to Buy and Sell Stock problem with examples and constraints

---
+++ day_002/solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    
+    # 121
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+        min_price = prices[0]
+        max_profit = 0
+        for price in prices[1:]:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
+        
+        return max_profit
