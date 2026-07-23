c450909dca41a4f2505e20ccdd377dd96a6304c9
sontvh2071 <sonhoang2071.work@gmail.com>
2026-07-23 14:57:17 +0700

    feat: add solution for Top K Frequent Elements problem with examples and constraints

---
+++ day_004/solution.py
@@ -0,0 +1,25 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if k == 1 and len(nums) == 1:
+            return nums
+        
+        m = {}
+        for num in nums:
+            m[num] = m.get(num, 0) + 1
+        
+        pq = []
+        for val, count in m.items():
+            heapq.heappush(pq, (-count, val))
+            
+        f = []
+        while pq and k:
+            e = heapq.heappop(pq)
+            f.append(e[1])
+            k -= 1
+        
+        return f
+        
