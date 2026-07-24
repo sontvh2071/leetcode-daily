a1fb9dec2dccc14e1283189076b6e741a9467aed
sontvh2071 <sonhoang2071.work@gmail.com>
2026-07-24 16:09:07 +0700

    feat: add solution for Isomorphic Strings problem with examples and constraints

--- day_005/solution.py
+++ day_005/solution.py
@@ -20,5 +20,18 @@ class Solution:
             i += 1
             
         return False
+    
+    # 205 isomorphic strings
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dict_s = {}
+        dict_t = {}
+        for i in range(len(s)):
+            if dict_s.get(s[i], None) is None and dict_t.get(t[i], None) is None:
+                dict_s[s[i]] = t[i]
+                dict_t[t[i]] = s[i]
             
+            elif dict_s.get(s[i], None) != t[i] and dict_t.get(t[i], None) != s[i]:
+                return False
         
+        return True
+                
